293ee998f6aad4bdedc9ca0f95b092e145634716
Derek Robati <derek.robati@gmail.com>
2021-01-27 19:24:01 -0500

    Starting point with generic client and server.

---
+++ server/module/index.js
@@ -0,0 +1,34 @@
+const { ApolloServer, gql } = require("apollo-server-fastify");
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "The Awakening",
+    author: "Kate Chopin",
+  },
+  {
+    title: "City of Glass",
+    author: "Paul Auster",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+module.exports = {
+  typeDefs,
+  resolvers,
+};
