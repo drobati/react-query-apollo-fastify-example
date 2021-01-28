6d8a7b141456738e5cefbdb0ad034aabd778499e
Derek Robati <derek.robati@gmail.com>
2021-01-27 20:47:47 -0500

    Updated schema and added mutation.

--- server/module/index.js
+++ server/module/index.js
@@ -1,30 +1,90 @@
 const { ApolloServer, gql } = require("apollo-server-fastify");
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
+  type Dessert {
+    Id: ID!
+    Name: String!
+    Calories: Int!
+    Fat: Int!
+    Carbs: Int!
+    Protein: Int!
   }
 
   type Query {
-    books: [Book]
+    Desserts: [Dessert]
+  }
+
+  type Mutation {
+    AddDessert(
+      Name: String!
+      Calories: Int!
+      Fat: Int!
+      Carbs: Int!
+      Protein: Int!
+    ): Dessert
   }
 `;
 
-const books = [
+const desserts = [
   {
-    title: "The Awakening",
-    author: "Kate Chopin",
+    Id: 0,
+    Name: "Oreo",
+    Calories: 437,
+    Fat: 18,
+    Carbs: 63,
+    Protein: 4,
   },
   {
-    title: "City of Glass",
-    author: "Paul Auster",
+    Id: 1,
+    Name: "Nougat",
+    Calories: 308,
+    Fat: 19,
+    Carbs: 9,
+    Protein: 37,
+  },
+  {
+    Id: 2,
+    Name: "Marshmellow",
+    Calories: 318,
+    Fat: 3,
+    Carbs: 81,
+    Protein: 2,
+  },
+  {
+    Id: 3,
+    Name: "Lollipop",
+    Calories: 398,
+    Fat: 2,
+    Carbs: 98,
+    Protein: 0,
+  },
+  {
+    Id: 4,
+    Name: "KitKat",
+    Calories: 518,
+    Fat: 26,
+    Carbs: 65,
+    Protein: 60,
   },
 ];
 
 const resolvers = {
   Query: {
-    books: () => books,
+    Desserts: () => desserts,
+  },
+  Mutation: {
+    AddDessert: async (_, { Name, Calories, Fat, Carbs, Protein }) => {
+      const newDessert = {
+        Id: desserts.length++,
+        Name,
+        Calories,
+        Fat,
+        Carbs,
+        Protein,
+      };
+      desserts.push(newDessert);
+      return newDessert;
+    },
   },
 };
 
